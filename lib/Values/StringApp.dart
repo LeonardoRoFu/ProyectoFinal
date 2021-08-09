0064c9940788e8480811551911e509f7b97d86e3
LeonardoRoFu <angel.rocha16@my.unitec.edu.mx>
2021-08-09 18:16:12 -0500

    Initial commit

---
+++ lib/Values/StringApp.dart
@@ -0,0 +1,39 @@
+
+//String Menu
+
+String get shopStr=> "EL BUEN CAFE";
+String get coffeesStr=> "Cafes";
+String get drinksStr => "Bebidas";
+String get cakesStr => "Pasteles";
+String get sandwichesStr => "Sandwiches";
+String get homeStr => "Explora";
+String get sectionMenuTitleStr => "Nuestra Carta";
+String get sectionMenuSubTitleStr => "Todo Organico";
+
+//String Drawer y footer
+String get aboutUsStr => "Nosotros";
+String get contactUsStr => "Contactanos";
+String get locationStr => "Ubicacion";
+String get knowUsStr => "Conocenos";
+String get loginStr => "Iniciar Sesión";
+String get copyrightStr => "RUIZ ASCENCIO YETLANEZI GUADALUPE | ROCHA FUENTES ANGEL LEONARDO";
+String get emailStr => "Correo";
+String get addressStr => "Direccion";
+String get paymentStr => "Pagos";
+String get cancellationStr => "Cancelación";
+String get fAQStr => "Preguntas frecuentes";
+
+//titles footer
+String get aboutStr => "SOBRE";
+String get helpStr => "AYUDA";
+String get socialStr => "REDES SOCIALES";
+
+//SOCIAL MEDIA
+String get twitterStr => "Twitter";
+String get facebookStr => "Facebook";
+String get instagramStr => "Instagram";
+
+//Datos default
+String get nameStr => "Yetlanezi Ruiz";
+String get emailDefaultStr => "yetlaneziruiz@elbuencafe.com";
+String get addressDefaultStr => "Av. Cappuccino #1234 col. Cafe";
